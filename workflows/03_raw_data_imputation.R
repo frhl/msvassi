fdfe32a1aa396ef9fd78d8a7085c4ccec5a1119d
frhl <frederik.heymann@gmail.com>
2021-07-14 22:39:38 +0200

    updated plots

--- workflows/03_raw_data_imputation.R
+++ workflows/03_raw_data_imputation.R
@@ -2,7 +2,7 @@
 source('scripts/impute_by_col.R')
 source('scripts/normalize_by_col.R')
 
-pdf('derived/tables/norm_imputation_graphs.pdf', width = 7, height = 5)
+#pdf('derived/tables/norm_imputation_graphs.pdf', width = 7, height = 5)
 
 # read in data
 df <- read.table('extdata/210714_ms_clean.txt', sep = '\t', header= T)
@@ -10,7 +10,7 @@ ints <- as.data.frame(df[,7:30])
 ints <- as.data.frame(sapply(ints, as.numeric))
 ints[ints==0] <- NA
 
-# log2, normalize and impute
+## log2, normalize and impute
 ints_df <- log2(ints)
 long = reshape2::melt(ints_df)
 p1 <- ggplot(long, aes(x = value)) +
@@ -19,14 +19,16 @@ p1 <- ggplot(long, aes(x = value)) +
   ggtitle('Log2 normalized')
 
 
-ints_df <- normalize_by_col(ints_df) # subtract the median
+## subtract the median
+ints_df <- normalize_by_col(ints_df) 
 long = reshape2::melt(ints_df)
 p2 <- ggplot(long, aes(x = value)) +
   geom_density() +
   facet_wrap(~variable) +
   ggtitle('With imputation + normalized')
+fwrite(ints_df, 'extdata/2101714_data_clean_no_imputation.txt', sep = '\t')
 
-## impute
+## impute left shifted gaussian
 ints_norm <- ints_df
 ints_df <- impute_by_col(ints_df) # left-shited gaussian
 long = reshape2::melt(ints_df)
@@ -35,15 +37,26 @@ p3 <- ggplot(long, aes(x = value)) +
   facet_wrap(~variable) +
   ggtitle('With imputation + normalized + imputed')
 
-print(p1)
-print(p2)
-print(p3)
+#print(p1)
+#print(p2)
+#print(p3)
 graphics.off()
 
+# load names
+d_names <- fread('derived/tables/datanames_new.csv')
+d_names$newname <- apply(d_names[,c(5,4)], 1, paste0, collapse = '_')
+conditions <- unique(d_names$V5)
+
 # add columns for imputed values
-for (cond in l$conditions){
+for (cond in conditions){
   print(cond)
-  bool_cond <- grepl(cond, colnames(ints))
-  ints_imputed <- ints[,bool_cond]
-  ints_df[[paste('Imputed',cond)]] <- rowSums(is.na(ints_imputed))
+  bool_cond <- grepl(cond, colnames(ints_norm))
+  ints_imputed <- ints_norm[,bool_cond]
+  ints_df[[paste0('Imputed_',cond)]] <- rowSums(is.na(ints_imputed))
 }
+
+# write data
+nrow(df)
+nrow(ints_df)
+outdf <- cbind(df[,!colnames(df) %in% d_names$newname], ints_df)
+fwrite(outdf, 'extdata/2101714_data_imputed_clean.txt', sep = '\t')
---
+++ workflows/04_correlation_matrix.R
@@ -0,0 +1,39 @@
+
+# load data
+#path <- 'extdata/2101714_data_clean_no_imputation.txt'
+path <- 'extdata/2101714_data_imputed_clean.txt'
+d <- fread(path, sep = '\t')
+
+# load names
+d_names <- fread('derived/tables/datanames_new.csv')
+d_names$newname <- apply(d_names[,c(5,4)], 1, paste0, collapse = '_')
+mat <- d[,d_names$newname, with = F]
+
+
+lst <- list()
+for (c1 in colnames(mat)){
+  lst[[c1]] <- list()
+  for(c2 in colnames(mat)){
+    cor1 <- mat[[c1]]
+    cor2 <- mat[[c2]]
+    #bool <- !is.na(cor1) | !is.na(cor2)
+    lst[[c1]][[c2]] <- cor(cor1, cor2)
+  }
+}
+
+
+# combine data into long format
+combine <- lapply(lst, stack)
+combine <- lapply(combine, function(x){colnames(x) = c('r','ind1'); return(x)})
+mat_r <- do.call(rbind, lapply(names(combine), function(name){combine[[name]]$ind2 = name; return(combine[[name]])}))
+mat_r$ind1 <- factor(mat_r$ind1, levels = d_names$newname)
+mat_r$ind2 <- factor(mat_r$ind2, levels = d_names$newname)
+
+ggplot(mat_r, aes(x = ind1, y = ind2, fill = r)) +
+  geom_tile() + theme_bw() + xlab('') + ylab('') +
+  scale_fill_gradient(low = 'white', high = 'red') +
+  labs(fill = 'Pearson correlation') +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+  ggtitle('Replicate correlation across experiments')
+ggsave('derived/plots/correlation_matrix_all_imputed.pdf')
+
